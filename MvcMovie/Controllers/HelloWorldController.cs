d9c29d87e619dc565ff7c3a25d508bf1bf8ae0bd
PMHC <pmhc91@gmail.com>
2018-01-25 22:00:15 +0100

    View Title Change

--- MvcMovie/Controllers/HelloWorldController.cs
+++ MvcMovie/Controllers/HelloWorldController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -11,15 +12,15 @@ namespace MvcMovie.Controllers
     public class HelloWorldController : Controller
     {
         //GET: /HelloWorld/
-        public string Index()
+        public IActionResult Index()
         {
-            return "This is my default action...";
+            return View();
         }
 
         //GET: /HelloWorl/Welcome/
-        public string Welcome()
+        public string Welcome(string name, int ID = 1)
         {
-            return HtmlEncoder
+            return HtmlEncoder.Default.Encode($"Hello {name}, ID: {ID}");
         }
     }
 }
